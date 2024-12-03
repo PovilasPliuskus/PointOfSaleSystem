ebafa2408e9d5a86601c807350e388563a6c7da0
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2024-12-03 20:54:52 +0200

    Fix Program.cs

--- PointOfSaleSystem.API/Program.cs
+++ PointOfSaleSystem.API/Program.cs
@@ -1,17 +1,25 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using PointOfSaleSystem.API.Context;
+using PointOfSaleSystem.API.Models.Entities;
+using PointOfSaleSystem.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<PointOfSaleSystemContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var mapperConfig = new MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<CompanyEntity, Company>();
+    cfg.CreateMap<Company, CompanyEntity>();
+});
+
+builder.Services.AddSingleton(mapperConfig.CreateMapper());
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<PointOfSaleSystemContext>();
-builder.Services.AddTransient<IMapper, Mapper>();
 
 var app = builder.Build();
 
