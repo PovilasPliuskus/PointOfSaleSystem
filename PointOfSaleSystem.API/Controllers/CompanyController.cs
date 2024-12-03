ed4b7d7f95074cdfbdb9b0ef1165ddcce5b01466
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2024-12-03 20:00:13 +0200

    Add template BusinessController
    Add BusinessController controller and write CRUD operations as a
    template.

---
+++ PointOfSaleSystem.API/Controllers/CompanyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PointOfSaleSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class CompanyController : ControllerBase
+    {
+        public CompanyController()
+        {
+
+        }
+
+        [HttpPost("business")]
+        public async Task<IActionResult> CreateBusiness()
+        {
+            return Ok();
+        }
+
+        [HttpGet("business")]
+        public async Task<IActionResult> GetBusinesses()
+        {
+            return Ok();
+        }
+
+        [HttpGet("business/{businessId}")]
+        public async Task<IActionResult> GetBusiness()
+        {
+            return Ok();
+        }
+
+        [HttpPut("business/{businessId}")]
+        public async Task<IActionResult> UpdateBusiness()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("business/{businessId}")]
+        public async Task<IActionResult> DeleteBusiness()
+        {
+            return Ok();
+        }
+    }
+}
